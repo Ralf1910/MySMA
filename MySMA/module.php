972c5eb72d73032ac18ab122058176c7bfbf443c
Ralf1910 <home@ralf-ehren.de>
2021-12-11 11:38:48 +0100

    d

--- MySMA/module.php
+++ MySMA/module.php
@@ -17,7 +17,7 @@ require_once(__MODULE__ . '/SMARegister.php');
  * @author      Frank Herrmann / Ralf Ehren
  *
  */
-class SMAModbus extends Module
+class SMA extends Module
 {
     use InstanceHelper;
 
